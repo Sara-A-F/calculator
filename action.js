515bb8b3f77a306a756b4f80f1506b7b64e76a3d
Sara-Ferrer <saraferrer@gmail.com>
2021-02-25 21:39:23 +0100

    Final

--- action.js
+++ action.js
@@ -1,29 +1,42 @@
-
 const buttons = document.querySelectorAll('button');
 
 const display = document.querySelector('.display');
- 
 
+ 
 buttons.forEach(function(button) {
   button.addEventListener('click', calculate);
 });
  
-
+// calculate function
 function calculate(event) {
- 
+
   const clickedButtonValue = event.target.value;
  
   if (clickedButtonValue === '=') {
-   
-    if (display.value !== '') {
     
+    if (display.value !== '') {
+   
       display.value = eval(display.value);
     }
-  } else if (clickedButtonValue === 'C') {
+  } else if (clickedButtonValue === 'DEL') {
+
+    display.value = display.value.substring(0, display.value.length - 1);
+
+  } else if (clickedButtonValue === '%') {
+
+    display.value = (eval(display.value)/100);  
   
+  } else if (clickedButtonValue === '+/-') {
+       
+        display.value = display.value * (-1) ; 
+    
+  } else if (clickedButtonValue === 'AC') {
+    
     display.value = '';
   } else {
-   
+    
     display.value += clickedButtonValue;
   }
 }
+
+
